c2341ebf98e798a03e1e65948f8eb3f681537494
pramodtielx-create <pramod.ti.elx@gmail.com>
2026-04-26 04:09:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import pandas as pd
 import json
 import requests
 from collections import defaultdict
+
 
 # After imports, before menu
 components.html("""
